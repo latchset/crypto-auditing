ffca392593d211fd9adea0db5d94cbdff41af3b6
Daiki Ueno <dueno@redhat.com>
2023-12-12 17:21:38 +0900

    agent: Clarify copyright and license
    This makes it explicit that BPF program sources (agent/src/bpf/*) are
    licensed under GPL-2.0-or-later instead of GPL-2.0-only.
    
    It also makes sure that the build scripts (agent/build.rs and
    agent/tests/agenttest/build.rs) are covered by GPL-3.0-or-later.
    
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- agent/tests/agenttest/src/bpf/agent.bpf.c
+++ agent/tests/agenttest/src/bpf/agent.bpf.c
@@ -1,4 +1,5 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* Copyright (C) 2023 The crypto-auditing developers. */
 
 #include "vmlinux.h"
 #include <bpf/usdt.bpf.h>
