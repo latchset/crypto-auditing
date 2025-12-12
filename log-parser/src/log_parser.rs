921f091bb2de9a86f27a2f77371be59ac907da33
Daiki Ueno <dueno@redhat.com>
2025-12-12 16:45:13 +0900

    log-parser: Print output through pager if needed
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- log-parser/src/log_parser.rs
+++ log-parser/src/log_parser.rs
@@ -3,12 +3,15 @@
 
 use anyhow::{Context as _, Result};
 use crypto_auditing::types::{ContextTracker, EventGroup};
+use pager::Pager;
 use serde_cbor::de::Deserializer;
+use std::io::{self, Write};
 
 mod config;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = config::Config::new()?;
+    Pager::new().skip_on_notty().setup();
 
     let log_file = std::fs::File::open(&config.log_file)
         .with_context(|| format!("unable to read file `{}`", config.log_file.display()))?;
@@ -18,6 +21,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         tracker.handle_event_group(&group?);
     }
     let root_contexts: Vec<_> = tracker.flush(None).into_iter().collect();
-    println!("{}", serde_json::to_string_pretty(&root_contexts).unwrap());
+    let content = serde_json::to_string_pretty(&root_contexts)?;
+    if let Err(e) = io::stdout().write_all(content.as_bytes()) {
+        if e.kind() != io::ErrorKind::BrokenPipe {
+            return Err(Box::new(e));
+        }
+    }
     Ok(())
 }
