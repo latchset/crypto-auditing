9c067c99e0ef5f880173238b631603a9b141d283
Daiki Ueno <dueno@redhat.com>
2026-01-25 12:59:10 +0900

    Move context tracking logic into the library
    Both log-parser and monitor previously had the almost identical
    context tracking logic. This merges them into a single structure,
    ContextTracker, in the library.
    
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- log-parser/src/log_parser.rs
+++ log-parser/src/log_parser.rs
@@ -3,12 +3,9 @@
 
 use anyhow::{Context as _, Result};
 use clap::Parser;
-use crypto_auditing::types::{Context, ContextID, Event, EventGroup};
+use crypto_auditing::types::{ContextTracker, EventGroup};
 use serde_cbor::de::Deserializer;
-use std::cell::RefCell;
-use std::collections::BTreeMap;
 use std::path::PathBuf;
-use std::rc::Rc;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -22,58 +19,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     let log_file = std::fs::File::open(&cli.log_path)
         .with_context(|| format!("unable to read file `{}`", cli.log_path.display()))?;
-    let mut all_contexts: BTreeMap<ContextID, Rc<RefCell<Context>>> = BTreeMap::new();
-    let mut root_contexts = Vec::new();
+    let mut tracker = ContextTracker::new();
     for group in Deserializer::from_reader(&log_file).into_iter::<EventGroup>() {
-        let group = group?;
-        for event in group.events() {
-            match event {
-                Event::NewContext {
-                    parent: parent_context,
-                    origin,
-                } => {
-                    let context = Rc::new(RefCell::new(Context {
-                        context: *group.context(),
-                        origin: origin.to_owned(),
-                        start: group.start(),
-                        end: group.end(),
-                        ..Default::default()
-                    }));
-                    if let Some(parent) = all_contexts.get(&parent_context[..]) {
-                        parent
-                            .borrow_mut()
-                            .spans
-                            .insert(*group.context(), context.clone());
-                    } else {
-                        root_contexts.push(context.clone());
-                    }
-                    all_contexts.insert(*group.context(), context);
-                }
-                Event::Data { key, value } => {
-                    if !all_contexts.contains_key(group.context()) {
-                        // Either this library did not do a new_context for this context, or the
-                        // log we have is truncated at the beginning. Just assume that this context
-                        // has no parent and create a new one so we don't loose the information in
-                        // this message.
-                        let context_obj = Rc::new(RefCell::new(Context {
-                            context: *group.context(),
-                            start: group.start(),
-                            end: group.end(),
-                            ..Default::default()
-                        }));
-                        root_contexts.push(context_obj.clone());
-                        all_contexts.insert(*group.context(), context_obj);
-                    }
-                    if let Some(parent) = all_contexts.get(group.context()) {
-                        parent
-                            .borrow_mut()
-                            .events
-                            .insert(key.to_string(), value.clone());
-                    }
-                }
-            }
-        }
+        tracker.handle_event_group(&group?);
     }
+    let root_contexts: Vec<_> = tracker.flush(None).into_iter().collect();
     println!("{}", serde_json::to_string_pretty(&root_contexts).unwrap());
     Ok(())
 }
