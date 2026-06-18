15de2ced952448d7d33942e66de0574e8debcc00
Daiki Ueno <dueno@redhat.com>
2026-06-18 15:53:10 +0900

    bpf: Work around bpf_stream_vprintk compatibility in libbpf 1.6
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- agent/tests/agenttest/src/bpf/agent.bpf.c
+++ agent/tests/agenttest/src/bpf/agent.bpf.c
@@ -2,6 +2,15 @@
 /* Copyright (C) 2023 The crypto-auditing developers. */
 
 #include "vmlinux.h"
+
+/* bpf_helpers.h from libbpf 1.6 had a conflicting declaration of
+ * bpf_stream_vprintk.
+ */
+#include <bpf/libbpf_version.h>
+#if defined(LIBBPF_MAJOR_VERSION) && defined(LIBBPF_MINOR_VERSION) && \
+  LIBBPF_MAJOR_VERSION == 1 && LIBBPF_MINOR_VERSION == 6
+# define bpf_stream_vprintk bpf_stream_vprintk_UNUSED
+#endif
 #include <bpf/usdt.bpf.h>
 
 #define MAX_DATA_SIZE 512
