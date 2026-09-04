47218d81e1707d9b19e2e2c331c1dff0e68f1d89
Daiki Ueno <dueno@redhat.com>
2026-09-04 15:29:10 +0900

    context_tracker: Factor out single event handling
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- crypto-auditing/src/context_tracker.rs
+++ crypto-auditing/src/context_tracker.rs
@@ -6,6 +6,7 @@ use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use sysinfo::System;
+use tracing::info;
 
 #[derive(Debug)]
 pub struct ContextTracker {
@@ -66,55 +67,78 @@ impl ContextTracker {
         self.boot_time.checked_add(time).unwrap_or(UNIX_EPOCH)
     }
 
+    // Returns `true` if a root context is created
+    fn handle_event(
+        &mut self,
+        id: &ContextId,
+        start: SystemTime,
+        end: SystemTime,
+        event: &Event,
+    ) -> Result<bool, Box<dyn std::error::Error>> {
+        match event {
+            Event::NewContext {
+                parent: parent_context,
+                origin,
+                executable,
+            } => {
+                let context = Rc::new(RefCell::new(Context {
+                    id: *id,
+                    origin: origin.to_owned(),
+                    executable: executable.to_owned(),
+                    start,
+                    end,
+                    ..Default::default()
+                }));
+                if let Some(parent) = self.last_context(&parent_context) {
+                    self.all_contexts.push(Rc::downgrade(&context));
+                    parent.borrow_mut().spans.push(context);
+                    Ok(false)
+                } else {
+                    self.all_contexts.push(Rc::downgrade(&context));
+                    self.root_contexts.push(context);
+                    Ok(true)
+                }
+            }
+            Event::Data { key, value } => {
+                if let Some(parent) = self.last_context(id) {
+                    parent
+                        .borrow_mut()
+                        .events
+                        .insert(key.to_string(), value.clone());
+                    Ok(false)
+                } else {
+                    // Either this library did not do a new_context for this context, or the
+                    // log we have is truncated at the beginning. Just assume that this context
+                    // has no parent and create a new one so we don't lose the information in
+                    // this message.
+                    let context_obj = Rc::new(RefCell::new(Context {
+                        id: *id,
+                        start,
+                        end,
+                        ..Default::default()
+                    }));
+                    self.all_contexts.push(Rc::downgrade(&context_obj));
+                    self.root_contexts.push(context_obj);
+                    Ok(true)
+                }
+            }
+        }
+    }
+
+    /// Returns the number of root contexts created from `group`
     pub fn handle_event_group(&mut self, group: &EventGroup) -> usize {
         let start = self.resolve_system_time(group.start());
         let end = self.resolve_system_time(group.end());
         let mut count = 0;
         for event in group.events() {
-            match event {
-                Event::NewContext {
-                    parent: parent_context,
-                    origin,
-                    executable,
-                } => {
-                    let context = Rc::new(RefCell::new(Context {
-                        id: *group.context(),
-                        origin: origin.to_owned(),
-                        executable: executable.to_owned(),
-                        start,
-                        end,
-                        ..Default::default()
-                    }));
-                    if let Some(parent) = self.last_context(&parent_context) {
-                        self.all_contexts.push(Rc::downgrade(&context));
-                        parent.borrow_mut().spans.push(context);
-                    } else {
-                        self.root_contexts.push(context);
+            match self.handle_event(group.context(), start, end, event) {
+                Ok(res) => {
+                    if res {
                         count += 1;
                     }
                 }
-                Event::Data { key, value } => {
-                    if let Some(parent) = self.last_context(group.context()) {
-                        parent
-                            .borrow_mut()
-                            .events
-                            .insert(key.to_string(), value.clone());
-                    } else {
-                        // Either this library did not do a new_context for this context, or the
-                        // log we have is truncated at the beginning. Just assume that this context
-                        // has no parent and create a new one so we don't lose the information in
-                        // this message.
-                        let context_obj = Rc::new(RefCell::new(Context {
-                            id: *group.context(),
-                            start,
-                            end,
-                            ..Default::default()
-                        }));
-                        self.all_contexts.push(Rc::downgrade(&context_obj));
-                        self.root_contexts.push(context_obj);
-                        count += 1;
-                    }
-                }
+                Err(e) => info!(error = %e, "error while handling event {:#?}",
+                                event),
             }
         }
         count
